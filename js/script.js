2b1daab0f2a8055c3e8b7de070efac0460f234de
Geovanny Gil <geovannygr2018@gmail.com>
2021-03-17 10:36:44 -0600

    Se creo todo la sección de inicio, con todos los archivos, con los archivos css incluidos y de igual manera ya se emepezo a trabajar con responsive

---
+++ js/script.js
@@ -0,0 +1,19 @@
+window.onscroll = function () {
+  let y = window.scrollY;
+  if (y > 20) {
+    document.getElementById("navbar").classList.add('sticky');
+  } else {
+    document.getElementById("navbar").classList.remove('sticky');
+  }
+}
+
+//Este es para el menu
+var barritasDeHamburguesa = document.getElementById('botonHamburguesa');
+var menuActivo = document.querySelector('.navbar .max-width ul.menu');
+var iconoCambio = document.querySelector('.navbar .max-width .menu-btn i');
+
+barritasDeHamburguesa.addEventListener("click", function () {
+  menuActivo.classList.toggle("active");
+  iconoCambio.classList.toggle("active");
+});
+
